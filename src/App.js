2ec2ebac170565e45f09d7090daf4f8ffff346f6
nimrodshn <nshneor@redhat.com>
2017-12-21 17:59:24 +0200

    second initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { LineChart } from 'patternfly-react';
+import { LineChart, Button } from 'patternfly-react';
 
 const lineChartDataColumns = [
     ['data1', 30, 200, 100, 400, 150, 250],
@@ -13,22 +13,48 @@ const lineChartConfigData = {
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.query = this.query.bind(this);
+  }
+
+
+  query(event) {
+    console.log("Querying.." + this.state.value);
+  }
+
   render() {
     return (
       <div className="App">
+        <form name="queryForm" id="query-form">
+          <div className="query">
+            <input id="query-text" value={this.state.value} />
+          </div>
+          <div className="query">
+            <Button
+              id="input-button"
+              onClick={this.query}
+            >
+              Execute
+            </Button>
+          </div>
+        </form>
+
         <div className="chart">
           <LineChart
-              id="line-chart-2"
-              type="spline"
-              data={lineChartConfigData}
-              grid={{
-                y: {
-                  show: false
-                },
-              }}
-              point={{
-                  show: false
-              }}
+            id="line-chart-2"
+            type="spline"
+            data={lineChartConfigData}
+            grid={{
+              y: {
+                show: false
+              },
+            }}
+            point={{
+              show: false
+            }}
           />
         </div>
       </div>
