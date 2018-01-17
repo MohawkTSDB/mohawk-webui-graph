babe814fe753a1161c33f5d861b51f0c56c902de
nimrodshn <nshneor@redhat.com>
2018-01-17 16:31:23 +0200

    removed authentication dropdown

--- src/ConfigurationTab.js
+++ src/ConfigurationTab.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {DEFAULT_URL} from './App';
 import {Button} from 'patternfly-react';
-import {DropdownButton, MenuItem} from 'react-bootstrap'
 
 class ConfigurationTab extends Component {
   constructor(props) {
@@ -37,14 +36,6 @@ class ConfigurationTab extends Component {
           <div className="url">
             <b>URL:</b>
             <input id="url" value={this.state.url} onChange={this.handleInputChange}/>
-          </div>
-          <div className="authentication">
-            <b>Authentication:</b>
-            <DropdownButton title={"Authentication"} onChange={this.handleAuthenticationSelect} id="authentication_dropdown">
-              <MenuItem eventKey="1">No Authentication</MenuItem>
-              <MenuItem eventKey="2">Basic Authentication</MenuItem>
-              <MenuItem eventKey="3">TLS Authentication</MenuItem>
-            </DropdownButton>
           </div>
         </form>
         <Button onClick={this.handleSaveConfiguration}>
